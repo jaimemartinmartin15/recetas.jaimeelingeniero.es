8d18260a36e2a19d3c8852b6b328f3aac25d120b
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2026-06-20 18:48:52 +0200

    Update folders structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { HomeComponent } from "../home/home.component";
 export const routes: Routes = [
   {
     path: "macarrones",
-    loadComponent: () => import("../recipes/lunch/macarrones/macarrones").then((m) => m.MacarronesComponent),
+    loadComponent: () => import("../recipes/macarrones/macarrones").then((m) => m.MacarronesComponent),
   },
   {
     path: "mouse-de-chocolate",
-    loadComponent: () => import("../recipes/desserts/mouse-de-chocolate/mouse-de-chocolate").then((m) => m.MouseDeChocolateComponent),
+    loadComponent: () => import("../recipes/mouse-de-chocolate/mouse-de-chocolate").then((m) => m.MouseDeChocolateComponent),
+  },
+  {
+    path: "sandwitch",
+    loadComponent: () => import("../recipes/sandwitch/sandwitch").then((m) => m.SandwitchComponent),
   },
   {
     path: "",
