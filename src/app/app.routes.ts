ac1ca2733141f94d4d1216765f6548d4bccce545
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2026-07-29 16:25:49 +0200

    Added sopa-maravilla and implement new design.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import { HomeComponent } from "../home/home.component";
 
 export const routes: Routes = [
   {
-    path: "sandwitch",
-    loadComponent: () => import("../recipes/sandwitch/sandwitch.component").then((m) => m.SandwitchComponent),
+    path: "sopa-maravilla",
+    loadComponent: () => import("../recipes/sopa-maravilla/sopa-maravilla.component").then((m) => m.SopaMaravillaComponent),
   },
-  {
-    path: "macarrones",
-    loadComponent: () => import("../recipes/macarrones/macarrones.component").then((m) => m.MacarronesComponent),
-  },
+  // {
+  //   path: "sandwitch",
+  //   loadComponent: () => import("../recipes/sandwitch/sandwitch.component").then((m) => m.SandwitchComponent),
+  // },
+  // {
+  //   path: "macarrones",
+  //   loadComponent: () => import("../recipes/macarrones/macarrones.component").then((m) => m.MacarronesComponent),
+  // },
   {
     path: "",
     pathMatch: "full",
