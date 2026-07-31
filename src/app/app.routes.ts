1a83ef0b046fab9a469237b969a1d50eac0ea422
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2026-07-31 23:14:28 +0200

    Added meta tags config and seo service.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
-import { Routes } from "@angular/router";
-import { HomeComponent } from "../home/home.component";
+import { Routes } from '@angular/router';
+import { HeadTags } from '@jaimemartinmartin15/jei-devkit-angular-shared';
+import { HomeComponent } from '../home/home.component';
 
 export const routes: Routes = [
   {
-    path: "sopa-maravilla",
+    path: 'sopa-maravilla',
     children: [
-      { path: "", loadComponent: () => import("../recipes/sopa-maravilla/sopa-maravilla.component").then((m) => m.SopaMaravillaComponent) },
-      { path: "**", redirectTo: "" },
+      {
+        path: '',
+        loadComponent: () => import('../recipes/sopa-maravilla/sopa-maravilla.component').then((m) => m.SopaMaravillaComponent),
+        data: {
+          headTags: {
+            metaDescription: 'Como preparar una sopa de lluvia con caldo de cocido.',
+            metaKeywords: ['sopa', 'maravilla', 'mercadona', 'caldo', 'cocido'],
+            linkRelCanonical: 'https://recetas.jaimeelingeniero.es/sopa-maravilla',
+          } as HeadTags,
+        },
+      },
+      { path: '**', redirectTo: '' },
     ],
   },
-  { path: "", pathMatch: "full", component: HomeComponent },
-  { path: "**", redirectTo: "" },
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: '**', redirectTo: '' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SeoService } from '@jaimemartinmartin15/jei-devkit-angular-shared';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { RouterOutlet } from '@angular/router';
   template: `<router-outlet />`,
   styleUrl: 'app.scss',
 })
-export class App {}
+export class App {
+  public constructor(readonly seoService: SeoService) {
+    seoService.listenNavigationEvents();
+  }
+}
