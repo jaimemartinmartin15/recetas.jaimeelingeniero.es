856463884d90fe0b7c359cb9ee0b9de8caaa156e
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2026-07-31 17:35:58 +0200

    Fix route redirections.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,23 +4,11 @@ import { HomeComponent } from "../home/home.component";
 export const routes: Routes = [
   {
     path: "sopa-maravilla",
-    loadComponent: () => import("../recipes/sopa-maravilla/sopa-maravilla.component").then((m) => m.SopaMaravillaComponent),
+    children: [
+      { path: "", loadComponent: () => import("../recipes/sopa-maravilla/sopa-maravilla.component").then((m) => m.SopaMaravillaComponent) },
+      { path: "**", redirectTo: "" },
+    ],
   },
-  // {
-  //   path: "sandwitch",
-  //   loadComponent: () => import("../recipes/sandwitch/sandwitch.component").then((m) => m.SandwitchComponent),
-  // },
-  // {
-  //   path: "macarrones",
-  //   loadComponent: () => import("../recipes/macarrones/macarrones.component").then((m) => m.MacarronesComponent),
-  // },
-  {
-    path: "",
-    pathMatch: "full",
-    component: HomeComponent,
-  },
-  {
-    path: "**",
-    redirectTo: "",
-  },
+  { path: "", pathMatch: "full", component: HomeComponent },
+  { path: "**", redirectTo: "" },
 ];
